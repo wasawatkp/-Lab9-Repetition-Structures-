deeaf3439d8246dcfbb087db289176de460f042a
wasawatkp <wasawat.upper@gmail.com>
2024-12-14 19:51:15 +0700

    lab9_2 Clear

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,17 +2,36 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int N, int M){
+	if (N <= 0 || M <= 0)  cout << "Invalid input";
+	for (int i = 0; i < N; i++)
+	{
+		if (N <= 0 || M <= 0)  break;
+		for (int i = 0; i < M; i++)
+		{
+			if (N <= 0 || M <= 0) { cout << "Invalid input"; break;}
+			cout << "O";
+			
+			
+		}
+		
+		cout << "\n";
+		
+	}
+	
+
+}
 
 int main(){
 	
-	printO(2,2);
+	/*printO(2,2);
 	cout << "\n";
 	
 	printO(3,5);
-	cout << "\n";	
+	cout << "\n";	*/
 	
 	printO(5,3);
-	cout << "\n";	
+	cout << "\n";
 	
 	printO(0,3);
 	cout << "\n";	
